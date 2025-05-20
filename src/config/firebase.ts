3c16a27e721af3e3684bd4eb145ef0d7288b001d
Mihret Desalegn <124504706+MihretDe@users.noreply.github.com>
2025-05-20 22:44:01 +0300

    Update firebase.ts due to security issue

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAF_UbFxRT3NpKz6GAtPuznZBmlrGGsa70",
-  authDomain: "task-management-f38b4.firebaseapp.com",
-  projectId: "task-management-f38b4",
-  storageBucket: "task-management-f38b4.firebasestorage.app",
-  messagingSenderId: "141910669297",
-  appId: "1:141910669297:web:6177acfaa18b6f66efb650",
-  measurementId: "G-W3QLVMD5K1"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
